159e28849a90d99b3f400f61f6dc2aa7678a988c
unknown <nabincool20@gmail.com>
2025-02-13 13:29:32 +1300

    charts

---
+++ week10/piechart.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+label=  ["A","B","C","D"]
+values = [3,7,1,8]
+plt.pie(values, labels=label)
+
+plt.title('Pie chart Example')
+plt.show()
+
+
+
---
+++ week10/scatterplot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+categories=['A','B','C','D']
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+plt.scatter(x,y)
+plt.title('Scatter Plot Example')
+plt.show()
